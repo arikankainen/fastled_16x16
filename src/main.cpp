f0e9f80ac3bc214c779fa022fd0dec2507fdbb4e
Ari Kankainen <ari.kankainen@saunalahti.fi>
2023-09-03 15:15:01 +0300

    added functions

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,11 @@ void setPixel(uint8_t var[PIXELS_X][PIXELS_Y][COLORS], uint8_t x, uint8_t y, uin
 void updateDisplay(uint8_t var[PIXELS_X][PIXELS_Y][COLORS]);
 void setPixels(uint8_t var[PIXELS_X][PIXELS_Y][COLORS], uint8_t r, uint8_t g, uint8_t b, bool refresh);
 void clearPixels(uint8_t var[PIXELS_X][PIXELS_Y][COLORS], bool refresh);
+void setRect(uint8_t var[PIXELS_X][PIXELS_Y][COLORS], uint8_t x, uint8_t y, uint8_t x2, uint8_t y2, uint8_t r, uint8_t g, uint8_t b, bool refresh);
 
 void setup() {
-    // Serial.begin(9600);
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
     FastLED.setBrightness(1);
-
-    // for (uint8_t x = 0; x < 6; x++) {
-    //     for (uint8_t y = 0; y < 6; y++) {
-    //         setPixel(matrix, x, y, 255, 50, 255, false);
-    //     }
-    // }
 
     // setPixel(matrix, 13, 15, 0, 0, 255, false);
     // setPixel(matrix, 14, 15, 0, 255, 0, false);
@@ -37,9 +31,15 @@ void setup() {
     // updateDisplay(matrix);
 
     // setPixels(matrix, 255, 255, 0, true);
+    setRect(matrix, 5, 5, 9, 9, 255, 0, 150, true);
 }
 
 void loop() {
+    // for (uint8_t i = 0; i < 256; i++) {
+    //     FastLED.showColor(CHSV(i, 255, 255));
+    //     // delay(10);
+    // }
+
     // setPixels(matrix, 255, 255, 0, true);
     // delay(1000);
     // clearPixels(matrix, true);
@@ -59,6 +59,12 @@ void setPixel(uint8_t var[PIXELS_X][PIXELS_Y][COLORS], uint8_t x, uint8_t y, uin
 void updateDisplay(uint8_t var[PIXELS_X][PIXELS_Y][COLORS]) {
     for (uint8_t y = 0; y < PIXELS_Y; y++) {
         for (uint8_t x = 0; x < PIXELS_X; x++) {
+            // odd rows of led matrix run backwards:
+            // leds[0] =  right corner of the first row  (x: 15, y: 0)
+            // leds[15] = left corner of the first row   (x: 0, y: 0)
+            // leds[16] = left corder of the second row  (x: 0, y: 1)
+            // leds[31] = right corner of the second row (x: 15, y: 1)
+
             uint8_t col = (y % 2 == 0 ? PIXELS_X - (x + 1) : x);
             uint8_t row = y;
             uint8_t led = row * PIXELS_X + col;
@@ -88,6 +94,16 @@ void clearPixels(uint8_t var[PIXELS_X][PIXELS_Y][COLORS], bool refresh) {
             var[x][y][R] = 0;
             var[x][y][G] = 0;
             var[x][y][B] = 0;
+        }
+    }
+
+    if (refresh) updateDisplay(var);
+}
+
+void setRect(uint8_t var[PIXELS_X][PIXELS_Y][COLORS], uint8_t x, uint8_t y, uint8_t x2, uint8_t y2, uint8_t r, uint8_t g, uint8_t b, bool refresh) {
+    for (uint8_t yy = y; yy <= y2; yy++) {
+        for (uint8_t xx = x; xx <= x2; xx++) {
+            setPixel(var, xx, yy, r, g, b, false);
         }
     }
 
